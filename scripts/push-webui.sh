278ed4e3469c5cb445323f4d0fadd6b052e07a1c
Tomáš Olvecký <tomas.olvecky@gmail.com>
2026-04-08 19:20:57 +0200

    chore: Migrate `push-webui.sh`

--- scripts/push-webui.sh
+++ scripts/push-webui.sh
@@ -13,7 +13,15 @@ OUTPUT_FILE="${2:-/dev/stdout}"
 just build
 
 if [ "$TAG" != "dry-run" ]; then
-    OUTPUT=$(obelisk component push "target/wasm32-wasip2/release/webui_proxy.wasm" \
-        "docker.io/getobelisk/webui:$TAG")
+    TMP_TOML=$(mktemp -t webui-deployment-XXXXXX.toml)
+    trap "rm -f $TMP_TOML" EXIT
+    cat > "$TMP_TOML" <<EOF
+[[webhook_endpoint_wasm]]
+name = "webui_proxy"
+location = "$(pwd)/target/wasm32-wasip2/release/webui_proxy.wasm"
+routes = [{ methods = ["GET"], route = "/" }]
+EOF
+    OUTPUT=$(obelisk component push --deployment "$TMP_TOML" \
+        webui_proxy "oci://docker.io/getobelisk/webui:$TAG")
     echo -n $OUTPUT > $OUTPUT_FILE
 fi
