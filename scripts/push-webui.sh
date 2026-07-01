d4f85aeb9767e6c4ed82c0343417b73fdf248e20
Tomáš Olvecký <tomas.olvecky@gmail.com>
2026-07-01 23:05:51 +0200

    build: Fix `push-webui.sh`

--- scripts/push-webui.sh
+++ scripts/push-webui.sh
@@ -5,22 +5,13 @@
 set -exuo pipefail
 cd "$(dirname "$0")/.."
 
-which trunk # nix develop .#web --command
-
 TAG="$1"
 OUTPUT_FILE="${2:-/dev/stdout}"
 
 just build
 
 if [ "$TAG" != "dry-run" ]; then
-    TMP_TOML=$(mktemp -t webui-deployment-XXXXXX.toml)
-    trap "rm -f $TMP_TOML" EXIT
-    cat > "$TMP_TOML" <<EOF
-[[webhook_endpoint_wasm]]
-name = "webui_proxy"
-location = "$(pwd)/target/wasm32-wasip2/release/webui_proxy.wasm"
-routes = [{ methods = ["GET"], route = "/" }]
-EOF
+    TMP_TOML= deployment-for-push.toml
     OUTPUT=$(obelisk component push --deployment "$TMP_TOML" \
         webui_proxy "oci://docker.io/getobelisk/webui:$TAG")
     echo -n $OUTPUT > $OUTPUT_FILE
